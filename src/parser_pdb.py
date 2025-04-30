04f7cd31883a460e46800d36ddc322c63f7cbb7f
Homero <homero.sanchez-moreira@etu.umontpellier.fr>
2025-04-30 15:38:42 +0200

    add funtion for obtaining the sequence

--- src/parser_pdb.py
+++ src/parser_pdb.py
@@ -6,7 +6,13 @@ from Bio import BiopythonWarning
 file_directory = "../data/FRUSTRATION_TMENC/FRUSTRATION_ANALYSIS_TMENC_CAPSIDS/MONOMER_CHAINA/TmEnc_monomer_0.pdb"
 
 
-def return_sequence (file_directory) :
+'''
+controles a faire:
+le fichier PDB contient quelque chose qui n'est pas un AA, faut indiquer dans quelle ligne
+'''
+
+#fonction thar take return the residues sequence on a given PDB
+def return_sequence_3_letter_format (file_directory) :
     # Suppression des warnings non critiques
     warnings.simplefilter('ignore', BiopythonWarning)
     list = []
@@ -20,5 +26,24 @@ def return_sequence (file_directory) :
         list.append(residue.get_resname())
     return list
 
+#fonction that converte a list of 3 letter code AminoAcids to a string on 1 letter code AminoAcids
+def three_to_one (list_of_tree):
+    dic = {'ALA': 'A', 'ARG': 'R', 'ASN': 'N', 'ASP': 'D', 'CYS': 'C', 'GLN': 'Q', 'GLU': 'E', 'GLY': 'G', 'HIS': 'H','HSD': 'H',
+           'ILE': 'I', 'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'PHE': 'F', 'PRO': 'P', 'SER': 'S', 'THR': 'T', 'TRP': 'W',
+           'TYR': 'Y', 'VAL': 'V'}
 
-print(return_sequence(file_directory ))
+    seq = ''
+    count = 0
+    for residue in list_of_tree:
+
+        if residue in dic:
+            seq+=dic[residue]
+            count += 1
+        else:
+            warnings.warn(f"the residue number {count} is in a invalid format")
+            seq+='_'
+
+    return seq
+
+
+print(three_to_one(return_sequence_3_letter_format(file_directory )))
