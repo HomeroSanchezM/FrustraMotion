06e8855c1b41e3c83dc9db2858140e3cd7e25b57
Homero <homero.sanchez-moreira@etu.umontpellier.fr>
2025-05-01 17:37:30 +0200

    align 2 sequences

--- src/parser_pdb.py
+++ src/parser_pdb.py
@@ -1,14 +1,20 @@
 from Bio.PDB import PDBParser
+#for ignoring warning after parsing
 import warnings
 from Bio import BiopythonWarning
+#for using parameters during execution
 import sys
 import os
+# for protein alignment
+from Bio.Align import PairwiseAligner
+from Bio.Seq import Seq
 
-file_directory = "../data/FRUSTRATION_TMENC/FRUSTRATION_ANALYSIS_TMENC_CAPSIDS/MONOMER_CHAINA/TmEnc_monomer_0.pdb"
+#file_directory = "../data/FRUSTRATION_TMENC/FRUSTRATION_ANALYSIS_TMENC_CAPSIDS/MONOMER_CHAINA/TmEnc_monomer_0.pdb"
 
 '''
 Usage:
-python parser_pdb.py chemin/vers/fichier.pdb
+get the sequence of one PDB :python3 parser_pdb.py chemin/vers/fichier.pdb
+Get the seqeunce for two PBD and compare it : python3 parser_pdb.py chemin/vers/1erfichier.pdb parser_pdb.py chemin/vers/2emefichier.pdb
 '''
 
 #fonction thar take return the residues sequence on a given PDB
@@ -52,24 +58,58 @@ def three_to_one (list_of_tree):
 
     return seq
 
+#fonction that take 2 sequences and make an alignement.
+def align_seqs(SEQ1 , SEQ2):
+    seq1 = Seq(SEQ1)
+    seq2 = Seq(SEQ2)
+    # Init align
+    ALIGN = PairwiseAligner()
+    alignments = ALIGN.align(seq1, seq2)
+
+    # give the best align
+    best_alignment = alignments[0]
+    return best_alignment
+
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python parser_pdb.py path/to/your_file.pdb", file=sys.stderr)
+    if len(sys.argv) != 2 and len(sys.argv) != 3 :
+        print("Usage: python parser_pdb.py path/to/your_file.pdb \n or python3 parser_pdb.py chemin/vers/1erfichier.pdb parser_pdb.py chemin/vers/2emefichier.pdb", file=sys.stderr)
         sys.exit(1)
 
-    pdb_file = sys.argv[1]
+    elif len(sys.argv) ==2:
+        pdb_file = sys.argv[1]
+        if not os.path.isfile(pdb_file):
+            print(f"Error: File not found - {pdb_file}", file=sys.stderr)
+            sys.exit(1)
+        else :
+            three_letter_seq = return_sequence_3_letter_format(pdb_file)
+            one_letter_seq = three_to_one(three_letter_seq)
+            print("\nResults:")
+            print(f"Input PDB file: {pdb_file}")
+            print(f"Residue count: {len(three_letter_seq)}")
+            #print(f"3-letter sequence: {' '.join(three_letter_seq)}")
+            print(f"1-letter sequence: {one_letter_seq}")
 
-    if not os.path.isfile(pdb_file):
-        print(f"Error: File not found - {pdb_file}", file=sys.stderr)
-        sys.exit(1)
+    elif len(sys.argv) ==3:
+        pdb_file1 = sys.argv[1]
+        pdb_file2 = sys.argv[2]
+        if not os.path.isfile(pdb_file1):
+            print(f"Error: File not found - {pdb_file1}", file=sys.stderr)
+            sys.exit(1)
+        elif not os.path.isfile(pdb_file2):
+            print(f"Error: File not found - {pdb_file2}", file=sys.stderr)
+            sys.exit(1)
+        else :
+            three_letter_seq1 = return_sequence_3_letter_format(pdb_file1)
+            one_letter_seq1 = three_to_one(three_letter_seq1)
+            three_letter_seq2 = return_sequence_3_letter_format(pdb_file2)
+            one_letter_seq2 = three_to_one(three_letter_seq2)
+            alignment = align_seqs(one_letter_seq1, one_letter_seq2)
+            print("\nResults:")
+            print(f"Input PDB files: \n {pdb_file1} \n {pdb_file2}")
+            print(f"Residue count: \n {len(three_letter_seq1)} \n {len(three_letter_seq2)}")
+            print(f"alignment : \n{alignment}")
 
-    three_letter_seq = return_sequence_3_letter_format(pdb_file)
-    one_letter_seq = three_to_one(three_letter_seq)
-    print("\nResults:")
-    print(f"Input PDB file: {pdb_file}")
-    print(f"Residue count: {len(three_letter_seq)}")
-    print(f"3-letter sequence: {' '.join(three_letter_seq)}")
-    print(f"1-letter sequence: {one_letter_seq}")
+
 
 if __name__ == "__main__":
     main()
