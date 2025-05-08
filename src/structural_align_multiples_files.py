f49ba097aa14561fbba5f1dce909b4676acc2b17
Homero <homero.sanchez-moreira@etu.umontpellier.fr>
2025-05-08 19:40:39 +0200

    work in process in the funtion ave_each_monomer_as_pdb

--- src/structural_align_multiples_files.py
+++ src/structural_align_multiples_files.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from Bio.PDB import PDBIO, Structure, Model, Chain
 import os
 import re
+import gzip
+import shutil
 
 
 '''
@@ -23,10 +25,10 @@ python3 structural_align_multiples_files.py path/to/directory/ number_of_wanted_
 # delete non essential warning
 warnings.simplefilter('ignore', BiopythonWarning)
 
-#1. Extract the type and the names of all the pdb files of a given directory
+#1. Extract the type, the names of all the pdb files, and the id of all the chains of a given directory
 def extract_info_from_directory(pdb_directory):
     """
-    Take a directory path and extract encapsulin type and return files in a dictionary with the frame number as key.
+    Take a directory path and extract encapsulin type and return the files path in a dictionary with the frame number as key.
 
     Args:
         pdb_directory (str): Path to directory containing PDB files
@@ -34,6 +36,7 @@ def extract_info_from_directory(pdb_directory):
     Returns:
         tuple: (encapsulin_type, dictionary_of_files)
             dictionary_of_files: {enc_number: file_path}
+            dico_of_chain_id: {chain_number: chain_id}
 
     Raises:
         ValueError: If directory doesn't exist or contains no valid files
@@ -63,31 +66,221 @@ def extract_info_from_directory(pdb_directory):
     if not enc_type or not files_dict:
         raise ValueError(f"No valid encapsulin files found in {pdb_directory}")
 
-    return enc_type, files_dict
+    parser = PDBParser(QUIET=True)
+    chain_dict = {}
+    structure = parser.get_structure("encapsulin", files_dict[0])
+    for model in structure :
+        chains = list(model)
+        for i, chain in enumerate(chains, start=1):
+            chain_dict[int(i)] = chain
+
+
+
+    return enc_type, files_dict, chain_dict
+
+def extract_chain_to_temp(pdb_path, chain_id, temp_dir="../tmp"):
+    """Extract a single chain to a temporary file"""
+    os.makedirs(temp_dir, exist_ok=True)
+    output_path = os.path.join(temp_dir, f"chain_{chain_id}_{os.path.basename(pdb_path)}")
+
+    with open(pdb_path) as f_in, open(output_path, 'w') as f_out:
+        for line in f_in:
+            if line.startswith(('ATOM')):
+                # Split using whitespace (multiple spaces/tabs)
+                fields = line.split()
+                # Le champ chain_id est normalement le 5ème après split()
+                # Mais cela dépend du format exact - peut nécessiter ajustement
+                if len(fields) >= 5 and fields[4] == chain_id:
+                    f_out.write(line)
+
+    return output_path
 
 #2. Load the wanted monomer of each frame in a list
-def load_monomers(files_dict, monomer_number):
+def load_monomers(files_dict,chain_dict, monomer_number):
     """Take a dico of file path and a wanted monomer and load the monomer of each frame in a list"""
-    parser = PDBParser(QUIET=True)
     chains = []
+    temp_files = []
+    chain_id = chain_dict[int(monomer_number)].get_id()
     for frame_number, path in sorted(files_dict.items()):
-        structure = parser.get_structure("encapsulin", path)
+            print(f"Extracting chain {chain_id} from file {os.path.basename(path)} ")
+            temp_path = extract_chain_to_temp(path, chain_id)
+            temp_files.append(temp_path)
+            parser = PDBParser(QUIET=True)
+            structure = parser.get_structure("temp", temp_path)
+            chains.append(structure[0][chain_id])
+    # Clean up temporary files
+    for temp_file in temp_files:
+        os.remove(temp_file)
 
-        for model in structure :
-            chain = list(model)[int(monomer_number)]
-            chains.append(chain)
     return chains
+
+#3.
+def structural_alignment(monomers):
+    """Align all the monomers to the first one"""
+    super_imposer = Superimposer()
+
+     # Handle empty input
+    if not monomers:
+        return []
+
+    # Determine input type (Chain or Structure)
+    reference = monomers[0]
+    is_chain = hasattr(reference, 'get_atoms') and not hasattr(reference, 'get_chains')
+    is_structure = hasattr(reference, 'get_chains')
+    if not (is_chain or is_structure):
+        raise ValueError("Input must be either list of Chains or list of Structures")
+
+    aligned = [reference]
+    if is_chain :
+        # parse the CA atoms of the reference monomer
+        ref_atoms = []
+        for residue in reference:
+            for atom in residue:
+                if atom.id == 'CA':
+                    ref_atoms.append(atom)
+
+        for monomer in monomers[1:]:
+            # parse the CA atoms of the mobile monomers
+            mobile_atoms = []
+            for residue in monomer:
+                for atom in residue:
+                    if atom.id == 'CA':
+                        mobile_atoms.append(atom)
+            # Align to the reference
+            super_imposer.set_atoms(ref_atoms, mobile_atoms)
+            # apply the regulation to the mobile monomers
+            super_imposer.apply(monomer)
+            # add the regulated monomers to the list aligned
+            aligned.append(monomer)
+    else :
+        print("on rentre dans le else")
+        # parse the CA atoms of the reference monomer
+        ref_atoms = []
+        for model in reference:
+            for monomer in model:
+                for residue in monomer:
+                    for atom in residue:
+                        if atom.id == 'CA':
+                            ref_atoms.append(atom)
+
+        for struct in monomers[1:]:
+            # parse the CA atoms of the mobile monomers
+            mobile_atoms = []
+            for model in struct:
+                for monomer in model:
+                    for residue in monomer:
+                        for atom in residue:
+                            if atom.id == 'CA':
+                                mobile_atoms.append(atom)
+
+            # Align to the reference
+            super_imposer.set_atoms(ref_atoms, mobile_atoms)
+            # apply the regulation to the mobile monomers
+            super_imposer.apply(monomer)
+            # add the regulated monomers to the list aligned
+            aligned.append(monomer)
+
+    return aligned
+
+#4.
+def save_each_monomer_as_pdb(aligned_monomers, output_dir, enc_type1 , file_dict1,monomer_number, enc_type2 = None ,file_dict2 = None ):
+    """
+    Save each monomer in a file (atom number for 1 file is limited to 99999 by PDBIO)
+    Args:
+        aligned_monomers: List of chain object (Monomers)
+        output_dir
+        enc_type: Type of encapsulin (MtEnc/TmEnc) have to be generalised
+        enc_number: Number of the encapsulin file
+
+    """
+    # Create the output directory
+    os.makedirs(output_dir, exist_ok=True)
+    print(f"aligned monomere a {len(aligned_monomers)} monomers")
+    for i, monomer in enumerate(aligned_monomers, start=1)and frame_number, path in sorted(file_dict1.items()) :
+        if enc_type2 == None :
+            # Create the structure
+            structure = Structure.Structure(f"MONOMER_{i}")
+            model = Model.Model(0)
+            structure.add(model)
+
+            # Create a new chain
+            new_chain = Chain.Chain("A")
+
+            # add the residues of the monomer
+            for residue in monomer:
+                new_chain.add(residue)
+
+            model.add(new_chain)
+
+            # addapt the serial number of atoms
+            atom_number = 1
+            for atom in structure.get_atoms():
+                atom.set_serial_number(atom_number)
+                atom_number += 1
+            output_file = os.path.join(output_dir, f"{enc_type1}{frame_number}_monomer{monomer_number}.pdb")
+
+            # create de pdb
+            io = PDBIO()
+            io.set_structure(structure)
+            io.save(output_file)
+
+
+            print(f"Monomer {i} save in {output_file}")
+        else :
+
+            structure = Structure.Structure(f"MONOMER_{i}")
+            structure.add(monomer)
+
+
+            enc_type = {1:enc_type1, 2:enc_type2}
+            enc_number = {1:file_dict1, 2:file_dict2}
+            #output_file = os.path.join(output_dir, f"{enc_type[i]}{enc_number[i]}_monomer{monomer_number}.pdb")
+
+
+             # create de pdb
+            #io = PDBIO()
+            #io.set_structure(monomer)
+            #io.save(output_file)
+            print(f"Work in process")
+
+
+
+
+
 
 def main(pdb_directory, monomer_number):
 
     #1.
-    enc_type, files_dict = extract_info_from_directory(pdb_directory)
+    enc_type, files_dict, chain_dict = extract_info_from_directory(pdb_directory)
     print(f"Encapsulin type : {enc_type}")
-    print(f"{len(files_dict)} frames :")
-    for frame_number, path in sorted(files_dict.items()):
-        print(f"frame {frame_number}: {os.path.basename(path)}")
+    print(f"Number of Monomers by structure : {len(chain_dict)}")
+    print(f"Number of frames : {len(files_dict)} ")
+    #print(f"{len(files_dict)} frames :")
+    #for frame_number, path in sorted(files_dict.items()):
+    #    print(f"frame {frame_number}: {os.path.basename(path)}")
+
+    #1.1. create the output directory path
+    frustration_dir = f"FRUSTRATION_{enc_type.upper()}"
+    capsids_dir = f"{enc_type.upper()}_CAPSIDS"
+    results_dir = os.path.join("../results", frustration_dir, capsids_dir, f"{enc_type}_monomer_{monomer_number}_aligned_frames")
+
+    plots_dir = os.path.join("../plots", enc_type)
+
+    #1.2 create the repository if it not exist
+    os.makedirs(results_dir, exist_ok=True)
+    os.makedirs(plots_dir, exist_ok=True)
+
     #2
-    print(load_monomers(files_dict, monomer_number))
+    monomers = load_monomers(files_dict,chain_dict, monomer_number)
+    print(f"Loaded {len(monomers)} monomers")
+
+     # 3. Alignement structural
+    aligned = structural_alignment(monomers)
+    print("Structural alignment completed")
+
+    # 4. create aligned PDB files
+    save_each_monomer_as_pdb(aligned, results_dir, enc_type, files_dict, monomer_number)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3 :
